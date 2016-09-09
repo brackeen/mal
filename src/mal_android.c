8d692410e6e8810295ad25b6dd60b85106dfc10d
David Brackeen <brackeen@gmail.com>
2016-09-08 18:45:25 -0700

    Update copyright

--- src/mal_android.c
+++ src/mal_android.c
@@ -1,6 +1,7 @@
 /*
  mal
- Copyright (c) 2014 David Brackeen
+ https://github.com/brackeen/mal
+ Copyright (c) 2014-2016 David Brackeen
 
  This software is provided 'as-is', without any express or implied warranty.
  In no event will the authors be held liable for any damages arising from the
