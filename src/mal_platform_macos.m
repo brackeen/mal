1d2abb4e9570dadac25ae1f11a0935603b574052
David Brackeen <brackeen@gmail.com>
2017-09-04 19:43:11 -0700

    macOS: Check routes

--- src/mal_platform_macos.m
+++ src/mal_platform_macos.m
@@ -23,21 +23,148 @@
 #include <TargetConditionals.h>
 #endif
 #if TARGET_OS_OSX
-
+#include <IOKit/audio/IOAudioTypes.h> // For terminal types
 #include "mal_audio_coreaudio.h"
 
-// TODO: macOS version is incomplete
+static void _malCheckRoutes(MalContext *context) {
+    if (!context) {
+        return;
+    }
+
+    memset(context->routes, 0, sizeof(context->routes));
+
+    AudioObjectPropertyAddress  propertyAddress;
+    AudioDeviceID defaultOutputDeviceID;
+    UInt32 propertySize;
+    AudioStreamID *streamIds = NULL;
+    UInt32 streamCount;
+    UInt32 transportType;
+    UInt32 terminalType;
+    OSStatus status = noErr;
+
+    // Get current output device
+    propertyAddress.mSelector = kAudioHardwarePropertyDefaultOutputDevice;
+    propertyAddress.mScope = kAudioObjectPropertyScopeGlobal;
+    propertyAddress.mElement = kAudioObjectPropertyElementMaster;
+    propertySize = sizeof(defaultOutputDeviceID);
+    status = AudioObjectGetPropertyData(kAudioObjectSystemObject, &propertyAddress, 0, NULL,
+                                        &propertySize, &defaultOutputDeviceID);
+    if (status != noErr) {
+        goto done;
+    }
+
+    // Get transport type
+    propertyAddress.mSelector = kAudioDevicePropertyTransportType;
+    propertyAddress.mScope = kAudioObjectPropertyScopeOutput;
+    propertyAddress.mElement = kAudioObjectPropertyElementMaster;
+    propertySize = sizeof(transportType);
+    status = AudioObjectGetPropertyData(defaultOutputDeviceID, &propertyAddress, 0, NULL,
+                                        &propertySize, &transportType);
+    if (status == noErr) {
+        // Quick exit if wireless
+        if (transportType == kAudioDeviceTransportTypeBluetooth ||
+            transportType == kAudioDeviceTransportTypeBluetoothLE ||
+            transportType == kAudioDeviceTransportTypeAirPlay) {
+            context->routes[MAL_ROUTE_WIRELESS] = true;
+            goto done;
+        }
+    }
+
+    // Get stream count
+    propertyAddress.mSelector = kAudioDevicePropertyStreams;
+    propertyAddress.mScope = kAudioDevicePropertyScopeOutput;
+    propertyAddress.mElement = kAudioObjectPropertyElementMaster;
+    propertySize = 0;
+    status = AudioObjectGetPropertyDataSize(defaultOutputDeviceID, &propertyAddress, 0, NULL,
+                                            &propertySize);
+    if (status != noErr) {
+        goto done;
+    }
+
+    streamCount = propertySize / sizeof(AudioStreamID);
+    if (streamCount == 0) {
+        goto done;
+    }
+
+    // Get stream ids
+    streamIds = malloc(propertySize);
+    if (!streamIds) {
+        goto done;
+    }
+    status = AudioObjectGetPropertyData(defaultOutputDeviceID, &propertyAddress, 0, NULL,
+                                        &propertySize, streamIds);
+    if (status != noErr) {
+        goto done;
+    }
+
+    // Get routes
+    for (UInt32 i = 0; i < streamCount; i++) {
+        propertyAddress.mSelector = kAudioStreamPropertyTerminalType;
+        propertyAddress.mScope = kAudioObjectPropertyScopeOutput;
+        propertyAddress.mElement = kAudioObjectPropertyElementMaster;
+        propertySize = sizeof(terminalType);
+
+        status = AudioObjectGetPropertyData(streamIds[i], &propertyAddress, 0, NULL,
+                                            &propertySize, &terminalType);
+        if (status != noErr) {
+            continue;
+        }
+
+        // Some devices use the terminal type enums defined in IOKit/audio/IOAudioTypes.h,
+        // others use the terminal type enums defined in CoreAudio/AudioHardwareBase.h.
+        if (terminalType == kAudioStreamTerminalTypeReceiverSpeaker) {
+            context->routes[MAL_ROUTE_RECIEVER] = true;
+        } else if (terminalType == kAudioStreamTerminalTypeHeadphones ||
+                   terminalType == OUTPUT_HEADPHONES ||
+                   terminalType == OUTPUT_HEAD_MOUNTED_DISPLAY_AUDIO) {
+            context->routes[MAL_ROUTE_HEADPHONES] = true;
+        } else if (terminalType == kAudioStreamTerminalTypeLine ||
+                   terminalType == kAudioStreamTerminalTypeDigitalAudioInterface ||
+                   terminalType == kAudioStreamTerminalTypeHDMI ||
+                   terminalType == kAudioStreamTerminalTypeDisplayPort) {
+            context->routes[MAL_ROUTE_LINEOUT] = true;
+        } else {
+            context->routes[MAL_ROUTE_SPEAKER] = true;
+        }
+    }
+
+done:
+    free(streamIds);
+}
+
+static OSStatus _malNotificationHandler(AudioObjectID inObjectID,
+                                        UInt32 inNumberAddresses,
+                                        const AudioObjectPropertyAddress inAddresses[],
+                                        void *inClientData) {
+    MalContext *context = inClientData;
+    _malCheckRoutes(context);
+    return noErr;
+}
 
 static void _malContextDidCreate(MalContext *context) {
-    // Do nothing
+    AudioObjectPropertyAddress  propertyAddress;
+    propertyAddress.mSelector = kAudioHardwarePropertyDefaultOutputDevice;
+    propertyAddress.mScope = kAudioObjectPropertyScopeGlobal;
+    propertyAddress.mElement = kAudioObjectPropertyElementMaster;
+
+    AudioObjectAddPropertyListener(kAudioObjectSystemObject, &propertyAddress,
+                                   &_malNotificationHandler, context);
 }
 
 static void _malContextWillDispose(MalContext *context) {
-    // Do nothing
+    AudioObjectPropertyAddress  propertyAddress;
+    propertyAddress.mSelector = kAudioHardwarePropertyDefaultOutputDevice;
+    propertyAddress.mScope = kAudioObjectPropertyScopeGlobal;
+    propertyAddress.mElement = kAudioObjectPropertyElementMaster;
+
+    AudioObjectRemovePropertyListener(kAudioObjectSystemObject, &propertyAddress,
+                                      &_malNotificationHandler, context);
 }
 
 static void _malContextDidSetActive(MalContext *context, bool active) {
-    // Do nothing
+    if (active) {
+        _malCheckRoutes(context);
+    }
 }
 
 #endif
