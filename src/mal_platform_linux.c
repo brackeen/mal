cc5ae1e4d9ccecbfb8f05a7c9b51ff914d1d66ba
David Brackeen <brackeen@gmail.com>
2018-01-05 18:20:52 -0800

    Linux: Remove duplicate function

--- src/mal_platform_linux.c
+++ src/mal_platform_linux.c
@@ -23,11 +23,6 @@
 
 #include "mal_audio_pulseaudio.h"
 
-void malContextPollEvents(MalContext *context) {
-    (void)context;
-    // Do nothing
-}
-
 static void _malContextCheckRoutes(MalContext *context) {
     (void)context;
     // Do nothing
